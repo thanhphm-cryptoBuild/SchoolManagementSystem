e27e0af2a388e083ec03ebfd8af9ca3d162b8242
truonganh <t.a.realestate52@gmail.com>
2024-09-08 20:10:16 +0700

    Change Layout

--- src/main/java/com/app/schoolmanagementsystem/application/Application.java
+++ src/main/java/com/app/schoolmanagementsystem/application/Application.java
@@ -11,8 +11,13 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/com/app/schoolmanagementsystem/views/Dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1028, 768);
+        stage.setResizable(false);
+        stage.setMinWidth(1028);
+        stage.setMinHeight(768);
         stage.setTitle("School Management System");
         stage.setScene(scene);
+        stage.centerOnScreen();
+
         stage.show();
     }
 
--- src/main/java/com/app/schoolmanagementsystem/controller/Controller.java
+++ src/main/java/com/app/schoolmanagementsystem/controller/Controller.java
@@ -1,11 +1,16 @@
 package com.app.schoolmanagementsystem.controller;
 
+import javafx.animation.TranslateTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,10 +18,74 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    private StackPane panepie;
+    private AnchorPane anchorSideBar;
+
+    @FXML
+    private ImageView close;
+
+    @FXML
+    private VBox iconAdvancedSB;
+
+    @FXML
+    private VBox iconCalendarSB;
+
+    @FXML
+    private VBox iconClassSB;
+
+    @FXML
+    private VBox iconDashboardSB;
+
+    @FXML
+    private VBox iconEventSB;
+
+    @FXML
+    private VBox iconStaffSB;
+
+    @FXML
+    private VBox iconStudentSB;
+
+    @FXML
+    private ImageView iconGifCar;
 
     @FXML
     private LineChart<?, ?> lineview;
+
+    @FXML
+    private VBox nameAdvancedSB;
+
+    @FXML
+    private VBox nameCalendarSB;
+
+    @FXML
+    private VBox nameClassSB;
+
+    @FXML
+    private VBox nameDashboardSB;
+
+    @FXML
+    private VBox nameEventSB;
+
+    @FXML
+    private VBox nameStaffSB;
+
+    @FXML
+    private VBox nameStudentSB;
+
+    @FXML
+    private Pane navigateSideBar;
+
+
+    @FXML
+    private VBox logo;
+
+    @FXML
+    private VBox logo_ref;
+
+    @FXML
+    private ImageView open;
+
+    @FXML
+    private StackPane panepie;
 
     @FXML
     private CategoryAxis x;
@@ -24,10 +93,170 @@ public class Controller implements Initializable {
     @FXML
     private NumberAxis y;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        anchorSideBar.setTranslateX(0);
+        anchorSideBar.toFront();
+        open.setVisible(false);
+        close.setVisible(true);
         loadDataPieChart();
         loadDataLineChart();
+    }
+
+    @FXML
+    void closeSideBar(MouseEvent event) {
+
+        TranslateTransition slide = new TranslateTransition();
+        slide.setDuration(Duration.seconds(0.05));
+        slide.setNode(anchorSideBar);
+        slide.setToX(-130);
+        slide.play();
+
+//        TranslateTransition iconSlideDashboard = new TranslateTransition(Duration.seconds(0.2), iconDashboardSB);
+//        iconSlideDashboard.setToX(130);
+//        nameDashboardSB.setVisible(false);
+//        iconSlideDashboard.play();
+//
+//        TranslateTransition iconSlideClass = new TranslateTransition(Duration.seconds(0.2), iconClassSB);
+//        iconSlideClass.setToX(130);
+//        nameClassSB.setVisible(false);
+//        iconSlideClass.play();
+//
+//        TranslateTransition iconSlideStudent = new TranslateTransition(Duration.seconds(0.2), iconStudentSB);
+//        iconSlideStudent.setToX(130);
+//        nameStudentSB.setVisible(false);
+//        iconSlideStudent.play();
+//
+//        TranslateTransition iconSlideStaff = new TranslateTransition(Duration.seconds(0.2), iconStaffSB);
+//        iconSlideStaff.setToX(130);
+//        nameStaffSB.setVisible(false);
+//        iconSlideStaff.play();
+//
+//        TranslateTransition iconSlideEvent = new TranslateTransition(Duration.seconds(0.2), iconEventSB);
+//        iconSlideEvent.setToX(130);
+//        nameEventSB.setVisible(false);
+//        iconSlideEvent.play();
+//
+//        TranslateTransition iconSlideAdvanced = new TranslateTransition(Duration.seconds(0.2), iconAdvancedSB);
+//        iconSlideAdvanced.setToX(130);
+//        nameAdvancedSB.setVisible(false);
+//        iconSlideAdvanced.play();
+//
+//        TranslateTransition iconSlideCalendar = new TranslateTransition(Duration.seconds(0.2), iconCalendarSB);
+//        iconSlideCalendar.setToX(130);
+//        nameCalendarSB.setVisible(false);
+//        iconSlideCalendar.play();
+//
+//        TranslateTransition navigateSB = new TranslateTransition(Duration.seconds(0.2), navigateSideBar);
+//        navigateSB.setToX(65);
+//        navigateSB.play();
+
+        hideIcons();
+
+        slide.setOnFinished((ActionEvent e) -> {
+            close.setVisible(false);
+            open.setVisible(true);
+        });
+    }
+
+    @FXML
+    void openSideBar(MouseEvent event) {
+        TranslateTransition slide = new TranslateTransition();
+        slide.setDuration(Duration.seconds(0.05));
+        slide.setNode(anchorSideBar);
+
+        slide.setToX(0);
+        slide.play();
+
+//        TranslateTransition iconSlideDashboard = new TranslateTransition(Duration.seconds(0.2), iconDashboardSB);
+//        iconSlideDashboard.setToX(0);
+//        nameDashboardSB.setVisible(true);
+//        iconSlideDashboard.play();
+//
+//        TranslateTransition iconSlideClass = new TranslateTransition(Duration.seconds(0.2), iconClassSB);
+//        iconSlideClass.setToX(0);
+//        nameClassSB.setVisible(true);
+//        iconSlideClass.play();
+//
+//        TranslateTransition iconSlideStudent = new TranslateTransition(Duration.seconds(0.2), iconStudentSB);
+//        iconSlideStudent.setToX(0);
+//        nameStudentSB.setVisible(true);
+//        iconSlideStudent.play();
+//
+//        TranslateTransition iconSlideStaff = new TranslateTransition(Duration.seconds(0.2), iconStaffSB);
+//        iconSlideStaff.setToX(0);
+//        nameStaffSB.setVisible(true);
+//        iconSlideStaff.play();
+//
+//        TranslateTransition iconSlideEvent = new TranslateTransition(Duration.seconds(0.2), iconEventSB);
+//        iconSlideEvent.setToX(0);
+//        nameEventSB.setVisible(true);
+//        iconSlideEvent.play();
+//
+//        TranslateTransition iconSlideAdvanced = new TranslateTransition(Duration.seconds(0.2), iconAdvancedSB);
+//        iconSlideAdvanced.setToX(0);
+//        nameAdvancedSB.setVisible(true);
+//        iconSlideAdvanced.play();
+//
+//        TranslateTransition iconSlideCalendar = new TranslateTransition(Duration.seconds(0.2), iconCalendarSB);
+//        iconSlideCalendar.setToX(0);
+//        nameCalendarSB.setVisible(true);
+//        iconSlideCalendar.play();
+//
+//        TranslateTransition navigateSB = new TranslateTransition(Duration.seconds(0.2), navigateSideBar);
+//        navigateSB.setToX(0);
+//        navigateSB.play();
+        showIcons();
+        slide.setOnFinished((ActionEvent e) -> {
+            close.setVisible(true);
+            open.setVisible(false);
+        });
+    }
+
+    private void hideIcons() {
+        nameDashboardSB.setVisible(false);
+        nameClassSB.setVisible(false);
+        nameStudentSB.setVisible(false);
+        nameStaffSB.setVisible(false);
+        nameEventSB.setVisible(false);
+        nameAdvancedSB.setVisible(false);
+        nameCalendarSB.setVisible(false);
+        logo.setVisible(false);
+        logo_ref.setVisible(true);
+
+        iconDashboardSB.setTranslateX(130);
+        iconClassSB.setTranslateX(130);
+        iconStudentSB.setTranslateX(130);
+        iconStaffSB.setTranslateX(130);
+        iconEventSB.setTranslateX(130);
+        iconAdvancedSB.setTranslateX(130);
+        iconCalendarSB.setTranslateX(130);
+        navigateSideBar.setTranslateX(65);
+        iconGifCar.setTranslateX(130);
+
+    }
+
+    private void showIcons() {
+        nameDashboardSB.setVisible(true);
+        nameClassSB.setVisible(true);
+        nameStudentSB.setVisible(true);
+        nameStaffSB.setVisible(true);
+        nameEventSB.setVisible(true);
+        nameAdvancedSB.setVisible(true);
+        nameCalendarSB.setVisible(true);
+        logo.setVisible(true);
+        logo_ref.setVisible(false);
+
+        iconDashboardSB.setTranslateX(0);
+        iconClassSB.setTranslateX(0);
+        iconStudentSB.setTranslateX(0);
+        iconStaffSB.setTranslateX(0);
+        iconEventSB.setTranslateX(0);
+        iconAdvancedSB.setTranslateX(0);
+        iconCalendarSB.setTranslateX(0);
+        navigateSideBar.setTranslateX(0);
+        iconGifCar.setTranslateX(0);
     }
 
     void loadDataPieChart() {
@@ -103,4 +332,5 @@ public class Controller implements Initializable {
 
         lineview.getData().addAll(lineChartOne, lineChartTwo, lineChartThree);
     }
+
 }
