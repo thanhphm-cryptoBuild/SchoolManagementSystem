186929af89e6eb728b9736d032f2096bafc5068a
ajixvedan123 <ajixvedan@gmail.com>
2024-09-09 14:58:39 +0700

    git

--- src/main/java/com/app/schoolmanagementsystem/application/Application.java
+++ src/main/java/com/app/schoolmanagementsystem/application/Application.java
@@ -1,28 +1,28 @@
-package com.app.schoolmanagementsystem.application;
+    package com.app.schoolmanagementsystem.application;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.Scene;
+    import javafx.stage.Stage;
 
-import java.io.IOException;
+    import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+    public class Application extends javafx.application.Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/com/app/schoolmanagementsystem/views/Dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1028, 768);
-        stage.setResizable(false);
-        stage.setMinWidth(1028);
-        stage.setMinHeight(768);
-        stage.setTitle("School Management System");
-        stage.setScene(scene);
-        stage.centerOnScreen();
+        @Override
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/com/app/schoolmanagementsystem/views/Dashboard.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1028, 768);
+            stage.setResizable(false);
+            stage.setMinWidth(1028);
+            stage.setMinHeight(768);
+            stage.setTitle("School Management System");
+            stage.setScene(scene);
+            stage.centerOnScreen();
 
-        stage.show();
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch();
+        }
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-}
