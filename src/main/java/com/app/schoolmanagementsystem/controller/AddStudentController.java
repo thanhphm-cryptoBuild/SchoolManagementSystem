6f1fc1acbd152a083c2cd8a1e39fe6310a9ae96f
truonganh <t.a.realestate52@gmail.com>
2024-09-11 13:02:04 +0700

    Update UI Staff, Student, Class: 60%

---
+++ src/main/java/com/app/schoolmanagementsystem/controller/AddStudentController.java
@@ -0,0 +1,15 @@
+package com.app.schoolmanagementsystem.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddStudentController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
