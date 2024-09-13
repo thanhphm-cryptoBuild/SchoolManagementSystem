5e753919375593c0551912aca2dbaefa5c29c246
truonganh <t.a.realestate52@gmail.com>
2024-09-13 15:12:01 +0700

    Update UI Staff, Student, Class: 90%

--- src/main/java/com/app/schoolmanagementsystem/controller/AddClassController.java
+++ src/main/java/com/app/schoolmanagementsystem/controller/AddClassController.java
@@ -1,12 +1,48 @@
 package com.app.schoolmanagementsystem.controller;
 
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddClassController implements Initializable {
 
+    @FXML
+    private StackPane formAddClass;
+
+    private StackPane pageClass;
+
+    @FXML
+    private AnchorPane moveBG;
+
+    public void setPageClass(StackPane pageClass) {
+        this.pageClass = pageClass;
+    }
+
+    public void setBGPageClass(AnchorPane moveBG) {
+        this.moveBG = moveBG;
+    }
+
+    @FXML
+    void closeFormAddClass(MouseEvent event) {
+        TranslateTransition translateTransition = new TranslateTransition();
+        translateTransition.setDuration(Duration.seconds(0.2));
+        translateTransition.setNode(formAddClass);
+        translateTransition.setToX(2000);
+
+        translateTransition.play();
+
+        translateTransition.setOnFinished(actionEvent -> {
+            pageClass.getChildren().remove(formAddClass);
+            moveBG.setEffect(null);
+        });
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
--- src/main/java/com/app/schoolmanagementsystem/controller/StaffController.java
+++ src/main/java/com/app/schoolmanagementsystem/controller/StaffController.java
@@ -4,7 +4,9 @@ import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
@@ -15,24 +17,35 @@ import java.util.ResourceBundle;
 public class StaffController implements Initializable {
 
     @FXML
+    private AnchorPane moveBG;
+
+    @FXML
     private StackPane pageStaff;
 
     @FXML
     void addStaffBTN(MouseEvent event) throws IOException {
-        StackPane pageAddStaff = FXMLLoader.load(getClass().getResource("/com/app/schoolmanagementsystem/views/PageAddStaff.fxml"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/app/schoolmanagementsystem/views/PageAddStaff.fxml"));
+        StackPane pageAddStaff = loader.load();
+
+        AddStaffController addStaffController = loader.getController();
+        addStaffController.setPageStaff(pageStaff);
+        addStaffController.setBGPageStaff(moveBG);
 
         pageAddStaff.setTranslateX(2000);
         pageAddStaff.setTranslateY(6);
 
-//        pageStudent.getChildren().removeAll();
         pageStaff.getChildren().add(pageAddStaff);
+
+        GaussianBlur gaussianBlur = new GaussianBlur(10);
+        moveBG.setEffect(gaussianBlur);
 
         TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setDuration(Duration.seconds(0.2));
         translateTransition.setNode(pageAddStaff);
         translateTransition.setFromX(2000);
         translateTransition.setToY(6);
-        translateTransition.setToX(500);
+        translateTransition.setToX(115);
 
         translateTransition.play();
     }
