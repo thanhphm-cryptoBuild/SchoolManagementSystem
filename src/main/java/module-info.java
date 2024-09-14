810617210d70999d93b695a362414d028c049e36
ajixvedan123 <ajixvedan@gmail.com>
2024-09-14 22:35:37 +0700

    oke

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,7 @@ module com.app.schoolmanagementsystem {
     requires javafx.fxml;
     requires java.sql;requires javafx.base;
     requires javafx.graphics;
-    requires jbcrypt;
-    requires java.mail;
+
 
     opens com.app.schoolmanagementsystem.model to javafx.base;
     opens com.app.schoolmanagementsystem.entities to javafx.base;
