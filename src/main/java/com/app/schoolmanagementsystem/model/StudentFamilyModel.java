510955b1e22b9d73126654ad7ef757086bd3b82a
ajixvedan123 <ajixvedan@gmail.com>
2024-09-14 14:08:34 +0700

    Them ClassModel, pageEditStudent.css, StudentFamilyModel, then default_avatar

---
+++ src/main/java/com/app/schoolmanagementsystem/model/StudentFamilyModel.java
@@ -0,0 +1,69 @@
+package com.app.schoolmanagementsystem.model;
+
+public class StudentFamilyModel {
+    private int familyID;
+    private int studentID;
+    private String fatherName;
+    private String fatherPhoneNumber;
+    private String motherName;
+    private String motherPhoneNumber;
+
+    // Constructor
+    public StudentFamilyModel(int familyID, int studentID, String fatherName, String fatherPhoneNumber, String motherName, String motherPhoneNumber) {
+        this.familyID = familyID;
+        this.studentID = studentID;
+        this.fatherName = fatherName;
+        this.fatherPhoneNumber = fatherPhoneNumber;
+        this.motherName = motherName;
+        this.motherPhoneNumber = motherPhoneNumber;
+    }
+
+    // Getters và Setters
+    public int getFamilyID() {
+        return familyID;
+    }
+
+    public void setFamilyID(int familyID) {
+        this.familyID = familyID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getFatherPhoneNumber() {
+        return fatherPhoneNumber;
+    }
+
+    public void setFatherPhoneNumber(String fatherPhoneNumber) {
+        this.fatherPhoneNumber = fatherPhoneNumber;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getMotherPhoneNumber() {
+        return motherPhoneNumber;
+    }
+
+    public void setMotherPhoneNumber(String motherPhoneNumber) {
+        this.motherPhoneNumber = motherPhoneNumber;
+    }
+}
