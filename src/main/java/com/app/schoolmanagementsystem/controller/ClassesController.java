e138a16b1431aee19a70058f29aa9a076ab22b11
truonganh <t.a.realestate52@gmail.com>
2024-09-17 16:32:09 +0700

    truongang_test_merge new

--- src/main/java/com/app/schoolmanagementsystem/controller/ClassesController.java
+++ src/main/java/com/app/schoolmanagementsystem/controller/ClassesController.java
@@ -23,33 +23,6 @@ public class ClassesController implements Initializable {
     @FXML
     private StackPane pageClass;
 
-    @FXML
-    void addClassBTN(MouseEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/app/schoolmanagementsystem/views/PageAddClass.fxml"));
-        StackPane pageAddClass = loader.load();
-
-        AddClassController addClassController = loader.getController();
-        addClassController.setPageClass(pageClass);
-        addClassController.setBGPageClass(moveBG);
-
-        pageAddClass.setTranslateX(2000);
-        pageAddClass.setTranslateY(10);
-
-//        pageStudent.getChildren().removeAll();
-        pageClass.getChildren().add(pageAddClass);
-
-        GaussianBlur gaussianBlur = new GaussianBlur(10);
-        moveBG.setEffect(gaussianBlur);
-
-        TranslateTransition translateTransition = new TranslateTransition();
-        translateTransition.setDuration(Duration.seconds(0.2));
-        translateTransition.setNode(pageAddClass);
-        translateTransition.setFromX(2000);
-        translateTransition.setToY(6);
-        translateTransition.setToX(440);
-
-        translateTransition.play();
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
